d83d371d480ceacaaf064d0a75cf07031ccb3dee
Aman Khan <amankgs94@gmail.com>
2017-11-21 16:33:56 -0800

    Update to work with python native
    more work needed before functional

--- Motion_Generation_Python/distance_3d.py
+++ Motion_Generation_Python/distance_3d.py
@@ -1,6 +1,8 @@
 def distance_3d(pt_A, pt_B):
-    x_del = pt_B[1] - pt_A[1];
-    y_del = pt_B[2] - pt_A[2];
-    z_del = pt_B[3] - pt_A[3];
+    x_del = pt_B[0] - pt_A[0];
+    y_del = pt_B[1] - pt_A[1];
+    z_del = pt_B[2] - pt_A[2];
+
+def leg():
     leg = (x_del**2 + y_del**2 + z_del**2)**(1/2);
     return(leg)
--- Motion_Generation_Python/motion_generator.py
+++ Motion_Generation_Python/motion_generator.py
@@ -1,5 +1,5 @@
 import numpy as np
-from distance_3d.ipynb import distance_3d
+from distance_3d import distance_3d
 
 pt_A = np.array([0.1, 0.1, 0.1]);
 pt_B = np.array([0.9, 0.8, 0.7]);
@@ -10,4 +10,4 @@ distance_3d(pt_A, pt_B)
 #def motion_generator(pt_A, pt_B, V_max):
 #    distance_3d(pt_A, pt_B);
 #    return leg
-print(leg)
+print(leg())
