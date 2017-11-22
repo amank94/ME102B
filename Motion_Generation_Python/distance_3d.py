76abda57f967543db9c229d994995d08475cc2b5
Aman Khan <amankgs94@gmail.com>
2017-11-21 16:52:20 -0800

    Fixed distance_3d error
    Do not use fractions lol

--- Motion_Generation_Python/distance_3d.py
+++ Motion_Generation_Python/distance_3d.py
@@ -3,6 +3,5 @@ def distance_3d(pt_A, pt_B):
     y_del = pt_B[1] - pt_A[1];
     z_del = pt_B[2] - pt_A[2];
 
-def leg():
-    leg = (x_del**2 + y_del**2 + z_del**2)**(1/2);
+    leg = (x_del**2 + y_del**2 + z_del**2)**(0.5);
     return(leg)
--- Motion_Generation_Python/motion_generator.py
+++ Motion_Generation_Python/motion_generator.py
@@ -5,9 +5,9 @@ pt_A = np.array([0.1, 0.1, 0.1]);
 pt_B = np.array([0.9, 0.8, 0.7]);
 V_max = 1;
 
-distance_3d(pt_A, pt_B)
+leg=distance_3d(pt_A, pt_B)
 
 #def motion_generator(pt_A, pt_B, V_max):
 #    distance_3d(pt_A, pt_B);
 #    return leg
-print(leg())
+print(leg)
