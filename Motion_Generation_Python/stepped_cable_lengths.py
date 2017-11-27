b5a819db5a700e9549917aaea7898c545d0afbb8
Aman Khan <amankgs94@gmail.com>
2017-11-27 11:46:05 -0800

    more fixes

--- Motion_Generation_Python/stepped_cable_lengths.py
+++ Motion_Generation_Python/stepped_cable_lengths.py
@@ -1,12 +1,12 @@
 def stepped_cable_lengths(ideal_cable_length, step_length):
     
     import numpy as np
-    import math
+    import math	
 
     
-    stepped_cables = np.zeros([8, len(ideal_cable_length)]);
+    stepped_cables = np.zeros([8, len(ideal_cable_length[0])]);
     
     for i in range (8):
-        stepped_cables[i,:] = math.ceil((ideal_cable_length[i,:] - ideal_cable_length[i,0])/step_length)*step_length + ideal_cable_length[i,0];
+        stepped_cables[i,:] = np.ceil((ideal_cable_length[i,:] - ideal_cable_length[i,0])/step_length)*step_length + ideal_cable_length[i,0];
         
         return stepped_cables
