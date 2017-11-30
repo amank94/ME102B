1acef7281b10b6cbdeabddafd6041ea4b947bfaa
Aman Khan <amankgs94@gmail.com>
2017-11-29 21:19:50 -0800

    fix t2 b2 (added wao)

--- Motion_Generation_Python/ideal_cable_length.py
+++ Motion_Generation_Python/ideal_cable_length.py
@@ -4,7 +4,7 @@ def ideal_cable_length(w,l,ht,hb,e,f,g,spool_ratio,cartesian_position):
     y_position = cartesian_position[1,:];
     z_position = cartesian_position[2,:];
 
-    winch_a=40.9
+    winch_a=40.9+2.2
     winch_b=44.6
     wao=200
 
@@ -21,13 +21,13 @@ def ideal_cable_length(w,l,ht,hb,e,f,g,spool_ratio,cartesian_position):
     #     )
     
     T1_cable_ideal = np.array(((x_position+wao-winch_a-f)**2 + (y_position+wao-winch_b-e)**2 + (ht-z_position-gt)**2)**(0.5));
-    T2_cable_ideal = np.array(((w-x_position-winch_b-f)**2 + (y_position+wao-winch_a-e)**2 + (ht-z_position-gt)**2)**(0.5));
-    T3_cable_ideal = np.array(((w-x_position-winch_a-f)**2 + (l-wao-y_position-winch_b-e)**2 + (ht-z_position-gt)**2)**(0.5));
+    T2_cable_ideal = np.array(((w-x_position-winch_b-f-wao)**2 + (y_position+wao-winch_a-e)**2 + (ht-z_position-gt)**2)**(0.5));
+    T3_cable_ideal = np.array(((w-x_position-winch_a-f-wao)**2 + (l-wao-y_position-winch_b-e)**2 + (ht-z_position-gt)**2)**(0.5));
     T4_cable_ideal = np.array(((x_position+wao-winch_b-f)**2 + (l-wao-y_position-winch_a-e)**2 + (ht-z_position-gt)**2)**(0.5));
     
     B1_cable_ideal = np.array(((x_position+wao-winch_a-f)**2 + (y_position+wao-winch_b-e)**2 + (hb-z_position-gb)**2)**(0.5));
-    B2_cable_ideal = np.array(((w-x_position-winch_b-f)**2 + (y_position+wao-winch_a-e)**2 + (hb-z_position-gb)**2)**(0.5));
-    B3_cable_ideal = np.array(((w-x_position-winch_a-f)**2 + (l-wao-y_position-winch_b-e)**2 + (hb-z_position-gb)**2)**(0.5));
+    B2_cable_ideal = np.array(((w-x_position-winch_b-f-wao)**2 + (y_position+wao-winch_a-e)**2 + (hb-z_position-gb)**2)**(0.5));
+    B3_cable_ideal = np.array(((w-x_position-winch_a-f-wao)**2 + (l-wao-y_position-winch_b-e)**2 + (hb-z_position-gb)**2)**(0.5));
     B4_cable_ideal = np.array(((x_position+wao-winch_b-f)**2 + (l-wao-y_position-winch_a-e)**2 + (hb-z_position-gb)**2)**(0.5));
 
     ideal_cable_delta = np.array([(T1_cable_ideal-T1_cable_ideal[0]),
--- Motion_Generation_Python/stepped_cable_lengths.py
+++ Motion_Generation_Python/stepped_cable_lengths.py
@@ -6,7 +6,8 @@ def stepped_cable_lengths(ideal_cable_length, step_length):
 
     
     for i in range (8):
-        stepped_cables[i,:] = np.ceil((ideal_cable_length[i,:] - ideal_cable_length[i,0])/step_length)*step_length + ideal_cable_length[i,0];
+        #stepped_cables[i,:] = np.ceil((ideal_cable_length[i,:] - ideal_cable_length[i,0])/step_length)*step_length + ideal_cable_length[i,0];
+        stepped_cables[i,:] = np.ceil((ideal_cable_length[i,:])/step_length)*step_length
     
     print('stepped cable lengths', stepped_cables)
     print('stepped cable length array shape', stepped_cables.shape)
