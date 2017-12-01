8e66f10fb0ebaabfe15c608946321d396524fdf7
ethanca <32251255+ethanca@users.noreply.github.com>
2017-12-01 00:46:58 -0800

    move code from cabli to github

--- Motion_Generation_Python/initialize_effector.py
+++ Motion_Generation_Python/initialize_effector.py
@@ -1,4 +1,15 @@
-def initialize_effector():
-	import RPi.GPIO as GPIO
-	c_axis = GPIO.PWM(8, 100) # pin 8, 100Hz
-	c_axis.start(15) #1.5 ms (center servo)
+
+from setup_sequence import setup_sequence
+from rotate_effector import rotate_effector
+import time
+setup_sequence()
+import RPi.GPIO as GPIO
+c_axis = GPIO.PWM(8, 100) # pin 8, 100Hz
+c_axis.start(20)
+#while(True):
+#    time.sleep(2)
+#    rotate_effector(0)
+#    print('forward')
+#    time.sleep(2)
+#    rotate_effector(120)
+#    print('backward')
--- Motion_Generation_Python/rotate_effector.py
+++ Motion_Generation_Python/rotate_effector.py
@@ -1,4 +1,11 @@
 def rotate_effector(c):
 	import RPi.GPIO as GPIO
 	c_axis = GPIO.PWM(8, 100) # pin 8, 100Hz
+        dc = 10 + (((float(c))/180)*10)
+        c_axis.start(dc)
+        print('c', c)
+        print('duty cycle', dc)
+            
+            
+	
 	
