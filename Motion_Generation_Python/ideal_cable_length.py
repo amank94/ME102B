366ed9b603d271a2f21a45954fdfbb50db5528b3
ethanca <32251255+ethanca@users.noreply.github.com>
2017-11-30 16:52:25 -0800

    kris kross

--- Motion_Generation_Python/ideal_cable_length.py
+++ Motion_Generation_Python/ideal_cable_length.py
@@ -8,6 +8,12 @@ def ideal_cable_length(w,l,ht,hb,e,f,g,spool_ratio,cartesian_position):
     winch_b=44.6
     wao=200
 
+
+    et=40
+    ft=59
+    eb=59
+    fb=40
+
     gt=98
     gb=53
 
@@ -20,15 +26,25 @@ def ideal_cable_length(w,l,ht,hb,e,f,g,spool_ratio,cartesian_position):
 
     #     )
     
-    T1_cable_ideal = np.array(((x_position+wao-winch_a-f)**2 + (y_position+wao-winch_b-e)**2 + (ht-z_position-gt)**2)**(0.5));
-    T2_cable_ideal = np.array(((w-x_position-winch_b-f-wao)**2 + (y_position+wao-winch_a-e)**2 + (ht-z_position-gt)**2)**(0.5));
-    T3_cable_ideal = np.array(((w-x_position-winch_a-f-wao)**2 + (l-wao-y_position-winch_b-e)**2 + (ht-z_position-gt)**2)**(0.5));
-    T4_cable_ideal = np.array(((x_position+wao-winch_b-f)**2 + (l-wao-y_position-winch_a-e)**2 + (ht-z_position-gt)**2)**(0.5));
+    # T1_cable_ideal = np.array(((x_position+wao-winch_a-f)**2 + (y_position+wao-winch_b-e)**2 + (ht-z_position-gt)**2)**(0.5));
+    # T2_cable_ideal = np.array(((w-x_position-winch_b-f-wao)**2 + (y_position+wao-winch_a-e)**2 + (ht-z_position-gt)**2)**(0.5));
+    # T3_cable_ideal = np.array(((w-x_position-winch_a-f-wao)**2 + (l-wao-y_position-winch_b-e)**2 + (ht-z_position-gt)**2)**(0.5));
+    # T4_cable_ideal = np.array(((x_position+wao-winch_b-f)**2 + (l-wao-y_position-winch_a-e)**2 + (ht-z_position-gt)**2)**(0.5));
     
-    B1_cable_ideal = np.array(((x_position+wao-winch_a-f)**2 + (y_position+wao-winch_b-e)**2 + (hb-z_position-gb)**2)**(0.5));
-    B2_cable_ideal = np.array(((w-x_position-winch_b-f-wao)**2 + (y_position+wao-winch_a-e)**2 + (hb-z_position-gb)**2)**(0.5));
-    B3_cable_ideal = np.array(((w-x_position-winch_a-f-wao)**2 + (l-wao-y_position-winch_b-e)**2 + (hb-z_position-gb)**2)**(0.5));
-    B4_cable_ideal = np.array(((x_position+wao-winch_b-f)**2 + (l-wao-y_position-winch_a-e)**2 + (hb-z_position-gb)**2)**(0.5));
+    # B1_cable_ideal = np.array(((x_position+wao-winch_a-f)**2 + (y_position+wao-winch_b-e)**2 + (hb-z_position-gb)**2)**(0.5));
+    # B2_cable_ideal = np.array(((w-x_position-winch_b-f-wao)**2 + (y_position+wao-winch_a-e)**2 + (hb-z_position-gb)**2)**(0.5));
+    # B3_cable_ideal = np.array(((w-x_position-winch_a-f-wao)**2 + (l-wao-y_position-winch_b-e)**2 + (hb-z_position-gb)**2)**(0.5));
+    # B4_cable_ideal = np.array(((x_position+wao-winch_b-f)**2 + (l-wao-y_position-winch_a-e)**2 + (hb-z_position-gb)**2)**(0.5));
+
+    T1_cable_ideal = np.array(((x_position+wao-winch_a-fb)**2 + (y_position+wao-winch_b-eb)**2 + (ht-z_position-gb)**2)**(0.5));
+    T2_cable_ideal = np.array(((w-x_position-winch_b-fb-wao)**2 + (y_position+wao-winch_a-eb)**2 + (ht-z_position-gb)**2)**(0.5));
+    T3_cable_ideal = np.array(((w-x_position-winch_a-fb-wao)**2 + (l-wao-y_position-winch_b-eb)**2 + (ht-z_position-gb)**2)**(0.5));
+    T4_cable_ideal = np.array(((x_position+wao-winch_b-fb)**2 + (l-wao-y_position-winch_a-eb)**2 + (ht-z_position-gb)**2)**(0.5));
+    
+    B1_cable_ideal = np.array(((x_position+wao-winch_a-ft)**2 + (y_position+wao-winch_b-et)**2 + (hb-z_position-gt)**2)**(0.5));
+    B2_cable_ideal = np.array(((w-x_position-winch_b-ft-wao)**2 + (y_position+wao-winch_a-et)**2 + (hb-z_position-gt)**2)**(0.5));
+    B3_cable_ideal = np.array(((w-x_position-winch_a-ft-wao)**2 + (l-wao-y_position-winch_b-et)**2 + (hb-z_position-gt)**2)**(0.5));
+    B4_cable_ideal = np.array(((x_position+wao-winch_b-ft)**2 + (l-wao-y_position-winch_a-et)**2 + (hb-z_position-gt)**2)**(0.5));
 
     ideal_cable_delta = np.array([(T1_cable_ideal-T1_cable_ideal[0]),
                              (T2_cable_ideal-T2_cable_ideal[0]),
