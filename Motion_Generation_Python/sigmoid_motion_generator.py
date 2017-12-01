f5e4ec4c4f60feb2fde5d41aa63946fc0945b3bc
Aman Khan <amankgs94@gmail.com>
2017-11-30 17:29:38 -0800

    remove redundant print statements

--- Motion_Generation_Python/sigmoid_motion_generator.py
+++ Motion_Generation_Python/sigmoid_motion_generator.py
@@ -24,7 +24,6 @@ def sigmoid_motion_generator(leg, V_max, A_max, V_tol, time_step):
     t = np.arange(0, 2*t[end_index], time_step);
 
     path_position = np.array(-leg/(1+np.exp(-a*(c-t))) + leg);
-    print('path position',path_position)
 
     #path_accel = np.array(-(2*leg*a**2.*np.exp(-2*a*(c - t)))/(np.exp(-a*(c - t)) + 1)**3 + (leg*a**2.*np.exp(-a*(c - t)))/(np.exp(-a*(c - t)) + 1)**2);
 
